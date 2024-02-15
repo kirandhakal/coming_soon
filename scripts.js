d0d3ad0867fbb867bb338c3ee0cc4cfd883ef0d1
kirandhakal <116267720+kirandhakal@users.noreply.github.com>
2024-02-15 09:26:37 +0545

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
 const box = document.getElementById('box');
 const text = document.getElementById('text');
+const heart =document.getElementById('heart');
 
 
  
 // Listen for the end of the animation
 box.addEventListener('animationend', function() {
-  text.style.opacity = '1'; // Show text after animation ends
+  text.style.opacity = '1';// Show text after animation ends
+   box.style.opacity='0'; 
+  heart.style.opacity='1';
+  heart.style.left='50%';
+  heart.style.top='30%';
 });
 
