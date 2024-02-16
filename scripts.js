d1c037aa1e6d55cdc7a12799e3c2ce0619308bf4
kirandhakal <116267720+kirandhakal@users.noreply.github.com>
2024-02-16 18:28:03 +0545

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,14 +2,15 @@ const box = document.getElementById('box');
 const text = document.getElementById('text');
 const heart =document.getElementById('heart');
 
-
  
 // Listen for the end of the animation
 box.addEventListener('animationend', function() {
-  text.style.opacity = '1';// Show text after animation ends
-   box.style.opacity='0'; 
+  text.style.opacity = '1';
+  box.style.opacity='0'; 
   heart.style.opacity='1';
-  heart.style.left='50%';
-  heart.style.top='30%';
+
 });
+console.log("Created by kirandhakal copyright © 2022|All right reserved|https://dhakalkiran.com.np/");
+
+
 
